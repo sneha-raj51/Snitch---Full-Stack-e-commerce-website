7e10461ff9ff2da62db0e748f9edaa50490107e9
sneha-raj <rajsanjoli51@gmail.com>
2026-08-29 00:12:07 +0530

    fix: set proxy true in google strategy for production

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -30,7 +30,8 @@ app.use(passport.initialize());
 passport.use(new GoogleStrategy({
     clientID: config.GOOGLE_CLIENT_ID,
     clientSecret: config.GOOGLE_CLIENT_SECRET,
-    callbackURL: "/api/auth/google/callback"
+    callbackURL: "/api/auth/google/callback",
+    proxy: true
 }, (accessToken, refreshToken, profile, done) => {
     return done(null, profile);
 }))
